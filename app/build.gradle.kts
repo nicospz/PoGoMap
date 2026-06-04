5ad014d68c3d7305986a94df9b9b7dc32e2d9f17
Nicolas Espinoza <niko.ep@gmail.com>
2026-06-04 19:39:43 +0900

    Bump version to 0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "dev.nicospz.pogomap"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["MAPS_API_KEY"] =
             providers.gradleProperty("MAPS_API_KEY").orElse(
